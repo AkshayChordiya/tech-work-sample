c721ef045114c61e323c99d9c6fd088e799544c8
Akshay Chordiya <akshaychordiya2@gmail.com>
2018-06-05 18:47:28 +0530

    Fixed small issue in JSON parsing

--- app/src/main/java/com/akshay/daresayweather/models/weather/Clouds.kt
+++ app/src/main/java/com/akshay/daresayweather/models/weather/Clouds.kt
@@ -3,5 +3,5 @@ package com.akshay.daresayweather.models.weather
 import com.google.gson.annotations.SerializedName
 
 data class Clouds(
-        @SerializedName("all") var all: Double = 0
+        @SerializedName("all") var all: Double = 0.0
 )
